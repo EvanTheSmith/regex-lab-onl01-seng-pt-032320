62b7679ba169b6b4f0024948380f426992436916
Evan Smith <professionalemail@zoho.com>
2020-04-06 19:15:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array=[]
+  text.each do |unending|
+  if unending.match(/\A["un"]\z["ending"]/)
+    array << unending
+  end
+  end
+  unending
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
